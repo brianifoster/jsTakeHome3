9eaea246b9577107044cd1e74b0ab20f87b948d1
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-01-30 23:50:32 -0800

    Finishes the first two steps in day4's Take Home assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-var errandOne = prompt("What are you going to do this weekend?");
+var errandOne = prompt("What are you doing this weekend?");
+var timeOne = prompt("How long will that take you?", "In hours");
 var errandTwo = prompt("What else are you going to do this weekend?");
+var timeTwo = prompt("And how long will that take?", "In hours");
 var errandThree = prompt("What final thing are you going to do this weekend?");
-var myErrands = 
+var timeThree = prompt("And finally, how long will that take?", "In hours");
+var myErrands = [];
+
+myErrands.push(errandOne, timeOne, errandTwo, timeTwo, errandThree, timeThree);
+// console.log(myErrands);
+
+for (let i = 0; i < myErrands.length; i++) {
+  if(myErrands[i] === errandOne) {
+    myErrands.splice(2, 0, 'Friday');
+  } else {
+    if(myErrands[i] === errandTwo) {
+      myErrands.splice(5, 0, 'Saturday');
+    } else {
+      if(myErrands[i] === errandThree) {
+        myErrands.splice(8, 0, 'Sunday');
+      }
+    }
+  }
+}
+console.log(myErrands);
