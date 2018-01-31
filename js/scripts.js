75342b3b2cf3f762b19e3a8af4fd02db08fb8d6c
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-01-30 21:53:18 -0800

    Makes my three prompts

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var errandOne = prompt("What are you going to do this weekend?");
+var errandTwo = prompt("What else are you going to do this weekend?");
+var errandThree = prompt("What final thing are you going to do this weekend?");
+var myErrands = 
