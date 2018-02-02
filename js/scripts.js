86e6b62d21fe3e9d1c04243e5c351e4ba8f88009
Brian Foster <hanai@Hanai-MacBook-Pro.local>
2018-02-01 18:38:30 -0800

    finished takeHome4 for sure

--- js/scripts.js
+++ js/scripts.js
@@ -34,3 +34,46 @@ var todoTwo = prompt("What else do you want to do this weekend?");
 var todoThree = prompt("What is a third thing you want to do this weekend?");
 
 todos.push(todoOne, todoTwo, todoThree);
+
+//STEP 2
+var todosWithDays = [];
+for(let i = 0; i < todos.length; i++) {
+  var due = prompt(`What day do you want to get ${todos[i]} done?`, 'Please use this format: day/month/year');
+  due = new Date(due);
+  todosWithDays.push([todos[i], due]);
+}
+
+//STEP 3
+var sortedWithIndices = [];
+let j = 0;
+var now = new Date().getTime();
+while (j < todosWithDays.length) {
+  let oneDay = 24 * 60 * 60 * 1000; // hourse*minutes*seconds*milliseconds
+  var diffDays = Math.round((todosWithDays[j][1].getTime() - now)/oneDay);
+  sortedWithIndices.push([diffDays, todosWithDays[j]]);
+  j++
+}
+
+sortedWithIndices.sort();
+
+console.log(sortedWithIndices[sortedWithIndices.length - 1][1], ' will take the longest');
+
+//STEP 4
+let n = 0;
+
+do {
+  if(n != sortedWithIndices.length - 1) {
+    sortedWithIndices[n][1].push("done");
+  }
+  n++
+} while(n < sortedWithIndices.length);
+
+let alertArr = [];
+for(let i = 0; i < sortedWithIndices.length; i++) {
+  if(sortedWithIndices[i][1].indexOf('done') !== -1) {
+    alertArr.push(sortedWithIndices[i][1]);
+  }
+}
+
+//STEP 5
+alert(alertArr);
